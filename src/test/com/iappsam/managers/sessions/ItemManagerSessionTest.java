0cef46959e3ef3f409153aef7faaa468ddfd65d0
jkdeveyra <JK@JK-PC>
2011-05-17 22:30:28 +0800

    Renamed sql scripts, refactored tests, fixed build script.

--- src/test/com/iappsam/managers/sessions/ItemManagerSessionTest.java
+++ src/test/com/iappsam/managers/sessions/ItemManagerSessionTest.java
@@ -1,45 +1,23 @@
 package com.iappsam.managers.sessions;
 
-
 import org.junit.After;
 import org.junit.Test;
 
-import com.iappsam.entities.Item;
-import com.iappsam.entities.ItemCondition;
-import com.iappsam.entities.ItemManagerFactory;
-import com.iappsam.entities.ItemStatus;
-import com.iappsam.entities.Unit;
-import com.iappsam.managers.ItemManager;
+import com.iappsam.entities.BasicItemEntities;
 import com.iappsam.managers.exceptions.TransactionException;
 
 public class ItemManagerSessionTest {
 
-	ItemManager im = new ItemManagerSession();
+	private BasicItemEntities entities = new BasicItemEntities(new ItemManagerSession());
 
 	@Test
 	public void addItemAndRemove() throws TransactionException {
-
-		ItemCondition condition = new ItemCondition("");
-		im.addItemCondition(condition);
-
-		ItemStatus status = new ItemStatus("");
-		im.addItemStatus(status);
-
-		Unit unit = new Unit("");
-		im.addUnit(unit);
-
-		Item item = ItemManagerFactory.createItemByName("Item");
-		im.addItem(item);
-
-		im.removeItem(item);
-		im.removeUnit(unit);
-		im.removeItemCondition(condition);
-		im.removeItemStatus(status);
+		entities.addAll();
+		entities.removeAll();
 	}
 
 	@After
 	public void removeAllAndClose() throws TransactionException {
-
-		im.close();
+		entities.close();
 	}
 }
