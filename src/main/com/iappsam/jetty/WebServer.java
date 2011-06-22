5497955607a92d415f53359f77d5ed70f904f175
jkdeveyra <viztha64@gmail.com>
2011-06-23 00:53:05 +0800

    Added PRLineServlet to WebServer

--- src/main/com/iappsam/jetty/WebServer.java
+++ src/main/com/iappsam/jetty/WebServer.java
@@ -1,5 +1,7 @@
 package com.iappsam.jetty;
 
+import javax.servlet.Servlet;
+
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.webapp.WebAppContext;
@@ -7,27 +9,33 @@ import org.eclipse.jetty.webapp.WebAppContext;
 import com.iappsam.servlet.LoginServlet;
 import com.iappsam.servlet.Menu;
 import com.iappsam.servlet.item.ItemServlet;
+import com.iappsam.servlet.pr.PRLineServlet;
 import com.iappsam.servlet.pr.PRServlet;
 
 public class WebServer {
 
+	private static WebAppContext context;
+
 	public static void main(String[] args) throws Exception {
 		Server server = new Server(80);
 
-		WebAppContext context = new WebAppContext();
+		context = new WebAppContext();
 		context.setResourceBase("webapp");
-		context.setDescriptor("webapp/WEB-INF/web.xml");
-		context.setContextPath("/");
 		context.setParentLoaderPriority(true);
 
-		context.addServlet(new ServletHolder(new LoginServlet()), "/login");
-		context.addServlet(new ServletHolder(new ItemServlet()), "/items");
-		context.addServlet(new ServletHolder(new PRServlet()), "/pr");
-		context.addServlet(new ServletHolder(new Menu()), "/menu");
+		addServlet(new LoginServlet(), "/login");
+		addServlet(new ItemServlet(), "/items");
+		addServlet(new PRServlet(), "/pr");
+		addServlet(new PRLineServlet(), "/pr/line");
+		addServlet(new Menu(), "/menu");
 
 		server.setHandler(context);
 
 		server.start();
 		server.join();
+	}
+
+	private static void addServlet(Servlet servlet, String path) {
+		context.addServlet(new ServletHolder(servlet), path);
 	}
 }
