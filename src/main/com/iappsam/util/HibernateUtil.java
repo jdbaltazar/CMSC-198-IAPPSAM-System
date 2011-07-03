31e287828a89b5ab958bcb4745567c6a750c7a53
John David Baltazar <johndavidbaltazar@yahoo.com.ph>
2011-07-03 23:11:10 +0800

    Warren's Saturday edit.

--- src/main/com/iappsam/util/HibernateUtil.java
+++ src/main/com/iappsam/util/HibernateUtil.java
@@ -72,7 +72,7 @@ public class HibernateUtil {
 		if (!tryToBuildSessionFactory("root", "123456"))
 			throw new RuntimeException("connection unsuccessful");
 
-//		try {
+//		try { 
 //			addDefaulEntities();
 //		} catch (TransactionException e) {
 //			e.printStackTrace();
@@ -86,7 +86,7 @@ public class HibernateUtil {
 			Properties p = new Properties();
 			p.setProperty("hibernate.connection.driver_class", "com.mysql.jdbc.Driver");
 			p.setProperty("hibernate.connection.url", "jdbc:mysql://localhost/iappsam");
-			p.setProperty("hibernate.show_sql", "true");
+			p.setProperty("hibernate.show_sql", "false");
 			p.setProperty("hibernate.connection.username", username);
 			p.setProperty("hibernate.connection.password", password);
 			p.setProperty("hibernate.search.default.indexBase", "./lucene-index");
